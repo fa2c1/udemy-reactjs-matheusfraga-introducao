2c13254176bb9c1f00a1638baca5ca69b2642ef4
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-13 12:13:31 -0300

    Aprendendo props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Equipe = (props) => {
+    return(
+        <>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+            <Social face={props.facebook} insta={props.instagram}/>
+        </>
+    );
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade} anos</h3>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <a href={props.face}>Facebook </a>
+            <a href={props.insta}>Instagram</a>
+        </div>
+    );
+}
+
+function App(){
+    return (
+    <div>
+        <h1>Conheça nossa equipe:</h1>
+        <Equipe nome="Fernando" cargo="Analista de Sistemas" idade="43" facebook="https://www.facebook.com/fa2c1st" instagram="https://www.instagram.com/fa2c1st"/>
+        <Equipe nome="Augusto" cargo="Engenheiro de Software" idade="44"facebook="https://www.facebook.com/fa2c1st" instagram="https://www.instagram.com/fa2c1st"/>
     </div>
-  );
+    );
 }
 
 export default App;
