364f665e604be78e50bb4e98832973de34790377
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-13 14:33:38 -0300

    Componentes de Classes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
-    return(
-        <>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-            <Social face={props.facebook} insta={props.instagram}/>
-        </>
-    );
+class Equipe extends Component{
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+                <hr />
+            </div>
+        );
+    }
 }
 
-const Sobre = (props) => {
-    return(
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade} anos</h3>
-        </div>
-    );
-}
-
-const Social = (props) => {
-    return(
-        <div>
-            <a href={props.face}>Facebook </a>
-            <a href={props.insta}>Instagram</a>
-        </div>
-    );
+class Sobre extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Olá sou o(a) {this.props.nome}</h2>
+                <h3>Cargo: {this.props.cargo}</h3>
+                <h3>Idade: {this.props.idade} anos</h3>
+            </div>
+        );
+    }
 }
 
 function App(){
     return (
     <div>
         <h1>Conheça nossa equipe:</h1>
-        <Equipe nome="Fernando" cargo="Analista de Sistemas" idade="43" facebook="https://www.facebook.com/fa2c1st" instagram="https://www.instagram.com/fa2c1st"/>
-        <Equipe nome="Augusto" cargo="Engenheiro de Software" idade="44"facebook="https://www.facebook.com/fa2c1st" instagram="https://www.instagram.com/fa2c1st"/>
+        <Equipe  nome="Fernando" cargo="Analista de Sistemas" idade="44" />
+        <Equipe  nome="Augusto" cargo="Engenheiro de software" idade="43" />
     </div>
     );
 }
